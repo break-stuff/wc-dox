c7e4203763de848a6202745e11627d5dbcbe3149
Burton Smith <burton@breakstuff.io>
2025-01-01 10:18:37 -0500

    remove default imports

--- src/configs/default-values.ts
+++ src/configs/default-values.ts
@@ -26,26 +26,7 @@ export const defaultDoxConfig: DoxConfig = {
     copiedIcon:
       '<?xml version="1.0" ?><svg style="enable-background:new 0 0 36 36;" version="1.1" viewBox="0 0 36 36" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Layer_1"/><g id="icons"><path class="st0" d="M12.8,28.7l-9.5-9.5c-0.4-0.4-0.4-1.1,0-1.6l1.5-1.5c0.4-0.4,1.1-0.4,1.6,0l7.2,7.2   l16-16c0.4-0.4,1.1-0.4,1.6,0l1.5,1.5c0.4,0.4,0.4,1.1,0,1.6L14.4,28.7C13.9,29.1,13.2,29.1,12.8,28.7z" id="check_x5F_mark_1_"/></g></svg>',
     copiedLabel: 'Import copied',
-    imports: [
-      {
-        label: 'HTML',
-        lang: 'html',
-        importTemplate: (tagName, className) =>
-          `<script type="module" src="https://cdn.jsdelivr.net/npm/my-library/dist/${tagName}/${className}.js"></script>`,
-      },
-      {
-        label: 'NPM',
-        lang: 'js',
-        importTemplate: (tagName, className) =>
-          `import 'my-library/dist/${tagName}/${className}.js';`,
-      },
-      {
-        label: 'React',
-        lang: 'js',
-        importTemplate: tagName =>
-          `import 'my-library/react/${tagName}/index.js';`,
-      },
-    ],
+    imports: [],
   },
   cssParts: {
     heading: 'CSS Parts',
